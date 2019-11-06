1fdf82253ca3edaddc0caaa7d3820b74d2cd971a
HEllicott <hannah.ellicott@os.uk>
2019-11-06 15:32:41 +0000

    working visit and delete buttons

--- js/main.js
+++ js/main.js
@@ -5,20 +5,50 @@ function saveBookmark(e) {
     // get form values
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
-    var new_bookmark = {
+    document.getElementById('bookmarkForm').reset();
+    var newBookmark = {
         name: siteName,
         url: siteUrl
     };
 
     if(localStorage.getItem('bookmarks') === null){
         var bookmarks = [];
-        bookmarks.push(new_bookmark);
+        bookmarks.push(newBookmark);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
     }else{
-        var current_bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-        current_bookmarks.push(new_bookmark)
-        localStorage.setItem('bookmarks', JSON.stringify(current_bookmarks))
+        var currentBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        currentBookmarks.push(newBookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(currentBookmarks));
     }
+    loadBookmarks()
     //prevent form from submitting before we've dealt with it
     e.preventDefault();
+}
+
+function deleteBookmark(url){
+    var currentBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for (let i = 0; i < currentBookmarks.length; i++) {
+        if(currentBookmarks[i].url === url){
+            currentBookmarks.splice(i, 1)
+        }
+    }
+    localStorage.setItem('bookmarks', JSON.stringify(currentBookmarks));
+    loadBookmarks()
+}
+
+function loadBookmarks() {
+    var currentBookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var savedBookmarks = document.getElementById('savedBookmarks');
+    savedBookmarks.innerHTML = '';
+    for(var i=0; i<currentBookmarks.length; i++){
+        var name = currentBookmarks[i].name;
+        var url = currentBookmarks[i].url;
+
+        savedBookmarks.innerHTML += '<div class="well">'+
+                                    '<h3>'+name+
+                                    '<a class="btn btn-info" target="_blank" href="'+url+'">Visit</a>'+
+                                    '<a class="btn btn-danger" onclick="deleteBookmark(\''+url+'\')" href="#">Delete</a>'+
+                                    '</h3>'+
+                                    '</div>'
+    }
 }
