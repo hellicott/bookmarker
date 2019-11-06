1a479b5ab1fa4d8d082a0695a72e54f57d00acc0
HEllicott <hannah.ellicott@os.uk>
2019-11-06 14:53:30 +0000

    saving to local storage

--- js/main.js
+++ js/main.js
@@ -5,12 +5,20 @@ function saveBookmark(e) {
     // get form values
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
-    var bookmark = {
+    var new_bookmark = {
         name: siteName,
         url: siteUrl
     };
 
-    console.log(bookmark);
+    if(localStorage.getItem('bookmarks') === null){
+        var bookmarks = [];
+        bookmarks.push(new_bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    }else{
+        var current_bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        current_bookmarks.push(new_bookmark)
+        localStorage.setItem('bookmarks', JSON.stringify(current_bookmarks))
+    }
     //prevent form from submitting before we've dealt with it
     e.preventDefault();
 }
